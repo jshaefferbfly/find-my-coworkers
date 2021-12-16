f3aac80bb228cb4f4601097f0c9d42cec871fb87
JD Shaeffer <jshaeffer@butterflynetinc.com>
2021-12-16 14:03:46 -0500

    more linting

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { LatLngExpression } from 'leaflet';
-import Map from './components/Map';
-import UserModal from './components/UserModal';
-import { useEffect, useState } from 'react';
-import './App.css';
+import { LatLngExpression } from "leaflet";
+import Map from "./components/Map";
+import UserModal from "./components/UserModal";
+import { useEffect, useState } from "react";
+import "./App.css";
 
 export interface UserData {
 	location: LatLngExpression;
@@ -13,19 +13,19 @@ export interface UserData {
 function App() {
 	const [me, setMe] = useState<UserData>({
 		location: [40.74344, -73.98725],
-		name: 'JD',
-		team: 'Cloud',
+		name: "JD",
+		team: "Cloud",
 	});
 	const [users, setUsers] = useState<UserData[]>([
 		{
 			location: [40.74344, -73.98725],
-			name: 'JD',
-			team: 'Cloud',
+			name: "JD",
+			team: "Cloud",
 		},
 		{
 			location: [40.74335, -73.98703],
-			name: 'Bob',
-			team: 'Cloud',
+			name: "Bob",
+			team: "Cloud",
 		},
 	]);
 
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,9 +1,9 @@
-import { useEffect } from 'react';
-import * as L from 'leaflet';
-import { MapContainer, Marker, TileLayer, Popup, Circle, CircleMarker } from 'react-leaflet';
-import { GestureHandling } from 'leaflet-gesture-handling';
-import { UserData } from '../App';
-import 'leaflet-gesture-handling/dist/leaflet-gesture-handling.css';
+import { useEffect } from "react";
+import * as L from "leaflet";
+import { MapContainer, Marker, TileLayer, Popup, Circle, CircleMarker } from "react-leaflet";
+import { GestureHandling } from "leaflet-gesture-handling";
+import { UserData } from "../App";
+import "leaflet-gesture-handling/dist/leaflet-gesture-handling.css";
 
 interface Props {
 	users: UserData[];
@@ -12,17 +12,17 @@ interface Props {
 
 const Map = ({ users, me }: Props) => {
 	useEffect(() => {
-		L.Map.addInitHook('addHandler', 'gestureHandling', GestureHandling);
+		L.Map.addInitHook("addHandler", "gestureHandling", GestureHandling);
 	}, []);
 
 	return (
 		<MapContainer center={me.location} zoom={15} scrollWheelZoom={false} gestureHandling={true}>
 			<TileLayer
 				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-				url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 			/>
 			<Marker position={me.location}>
-				<Popup className='popup' closeButton={false} autoClose={false} closeOnEscapeKey={false} closeOnClick={false}>
+				<Popup className="popup" closeButton={false} autoClose={false} closeOnEscapeKey={false} closeOnClick={false}>
 					{me.name} <br />
 					{me.team}
 				</Popup>
