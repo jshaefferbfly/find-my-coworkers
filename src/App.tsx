a578797ff2d15a52f00eaa19a837bdee1ecfb21e
JD Shaeffer <jshaeffer@butterflynetinc.com>
2021-12-16 10:54:53 -0500

    edits

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
+import { LatLngExpression } from 'leaflet';
+import { MapContainer, Marker, TileLayer, Popup } from 'react-leaflet'
 import './App.css'
-import L from 'leaflet'
 
 function App() {
-  const map = L.map('map').setView([51.505, -0.09], 13);
+  const position: LatLngExpression = [51.505, -0.09]
 
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <h1>yo</h1>
-        <div id='map'></div>
-      </header>
-    </div>
+    <>
+      <h1>yo</h1>
+      <MapContainer className='map' center={position} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </>
   );
 }
 
