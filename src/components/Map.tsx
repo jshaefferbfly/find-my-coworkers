30201ff8c5fa8e8ae682696e6a66e62c36d2047e
JD Shaeffer <jshaeffer@butterflynetinc.com>
2021-12-16 14:45:04 -0500

    avatars

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -2,8 +2,14 @@ import { useEffect } from "react";
 import * as L from "leaflet";
 import { MapContainer, Marker, TileLayer, Circle, Tooltip } from "react-leaflet";
 import { GestureHandling } from "leaflet-gesture-handling";
+import { createAvatar } from "@dicebear/avatars";
+import * as style from "@dicebear/pixel-art";
 import { UserData } from "../App";
 import "leaflet-gesture-handling/dist/leaflet-gesture-handling.css";
+
+const avatar = createAvatar(style, {
+	seed: "custom-seed",
+});
 
 interface Props {
 	users: UserData[];
@@ -28,8 +34,9 @@ const Map = ({ users, me }: Props) => {
 			<Marker position={me.location}>
 				<Tooltip permanent direction="top" offset={[-15, 30]} opacity={1} interactive={true}>
 					<div onClick={handleUserSelect}>
-						{me.name} <br />
-						{me.team}
+						<h4>{me.name}</h4>
+						<p>{me.team}</p>
+						<img src={`data:image/svg+xml;utf8,${encodeURIComponent(avatar)}`} />
 					</div>
 				</Tooltip>
 				<Circle center={me.location} radius={20} />
